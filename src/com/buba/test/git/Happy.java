8b38dff6c8e7e4c3298d44b32e9d0a361c429209
tom_glb <goodmorning_glb@atguigu.com>
2019-06-17 01:51:55 +0800

    test

--- src/com/buba/test/git/Happy.java
+++ src/com/buba/test/git/Happy.java
@@ -2,11 +2,7 @@ package com.buba.test.git;
 
 public class Happy {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("right-----right");
-=======
-		//System.out.println("right-----");
->>>>>>> branch 'master' of https://github.com/yunbuquf/gitTest.git
 		System.out.println("left-----");
 	}
 
