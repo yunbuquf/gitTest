2bcac2c9fd28d3c6418851592194f36d2d6b843c
tom_glb <2671400325@qq.com>
2019-06-17 01:47:46 +0800

    注释

--- src/com/buba/test/git/Happy.java
+++ src/com/buba/test/git/Happy.java
@@ -2,7 +2,7 @@ package com.buba.test.git;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("right-----");
+		//System.out.println("right-----");
 		System.out.println("left-----");
 	}
 
